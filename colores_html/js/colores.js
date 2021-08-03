9b9f9f056c01989ab254576309d2b00c72e9c3a6
reigert1986 <reigert_lorigador@outlook.com>
2021-08-03 15:05:31 -0300

    "html de colores"

---
+++ colores_html/js/colores.js
@@ -0,0 +1,28 @@
+/*
+   for(var i=0; i < 100 ;i++){
+       
+
+     var numero = Math.random()*99;              
+       numero = Math.floor(numero);
+       document.write(numero+ "<br>");
+   }
+  
+   document.write("hola" + "<br>");
+ 
+   let numero = 25;
+   document.write(numero);
+
+
+ let cadena = "hola pitufo ";
+ let cad = "anda a C!!!";
+
+ palabra = cadena.concat(cad);
+ document.write(palabra + "<br>");
+
+ document.write(cadena[6]+ "<br>");
+ */ 
+
+const p = document.querySelector(".nose");
+
+document.write(p.style.color = "blue");
+
